c70b668ea9b25d38f7d188603e1ecb66e799e41a
Toyem Ryan <S1089982@studenti.univpm.it>
2023-07-01 01:39:09 +0200

    front end touch

--- app/src/main/java/com/example/meteoapp/MainActivity.kt
+++ app/src/main/java/com/example/meteoapp/MainActivity.kt
@@ -2,41 +2,46 @@ package com.example.meteoapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
 import com.example.meteoapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         @Suppress("UNUSED_VARIABLE")
+
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        //put all the drawerlayout into the navigation, create the button in the appbar
-        //with layout_gravity we don't have superposition of view
+        //put all the drawerlayout into the appbarconfiguration
+        //with layout_gravity we don't have the superposition of the view
         drawerLayout = binding.drawerLayout
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
-        // configurate the navigationView to be used by the navController
-        NavigationUI.setupWithNavController(binding.navView, navController)
+        // configurate the toolbar with the navView to be used by the navController
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp(drawerLayout)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }
--- app/src/main/java/com/example/meteoapp/mainMeteo/MainMeteoFragment.kt
+++ app/src/main/java/com/example/meteoapp/mainMeteo/MainMeteoFragment.kt
@@ -2,11 +2,16 @@ package com.example.meteoapp.mainMeteo
 
 //import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.meteoapp.R
+import com.example.meteoapp.databinding.FragmentMainMeteoBinding
 
 class MainMeteoFragment : Fragment() {
 
@@ -16,8 +21,20 @@ class MainMeteoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main_meteo, container, false)
+    ): View {
+
+        val binding = DataBindingUtil.inflate<FragmentMainMeteoBinding>(inflater,
+            R.layout.fragment_main_meteo,container,false)
+
+        binding.Goto.setOnClickListener{  view : View ->
+            view.findNavController().navigate(R.id.action_mainMeteoFragment_to_loginFragment) }
+
+        val toolbar = (activity as AppCompatActivity).findViewById<Toolbar>(R.id.toolbar)
+
+        toolbar.title = "Ancona"
+
+
+        return binding.root
     }
 
 
