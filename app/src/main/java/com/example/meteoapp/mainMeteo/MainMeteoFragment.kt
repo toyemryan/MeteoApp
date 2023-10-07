ba00e065c1276f1799c0c8cd97e3ecb26878cce3
Toyem Ryan <S1089982@studenti.univpm.it>
2023-10-07 14:03:27 +0200

    login system navigation, animation

--- app/src/main/java/com/example/meteoapp/mainMeteo/MainMeteoFragment.kt
+++ app/src/main/java/com/example/meteoapp/mainMeteo/MainMeteoFragment.kt
@@ -1,9 +1,5 @@
 package com.example.meteoapp.mainMeteo
 
-<<<<<<< HEAD
-=======
-//import androidx.lifecycle.ViewModelProvider
->>>>>>> 66331d2c6b518470ccecdcd76e1d77bda278a5ee
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -11,10 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-<<<<<<< HEAD
-=======
-import androidx.navigation.findNavController
->>>>>>> 66331d2c6b518470ccecdcd76e1d77bda278a5ee
 import com.example.meteoapp.R
 import com.example.meteoapp.databinding.FragmentMainMeteoBinding
 import kotlin.math.abs
@@ -30,11 +22,8 @@ class MainMeteoFragment : Fragment() {
 
   //  private lateinit var viewModel: MainMeteoViewModel
 
-<<<<<<< HEAD
     //private lateinit var firebaseAuth: FirebaseAuth
 
-=======
->>>>>>> 66331d2c6b518470ccecdcd76e1d77bda278a5ee
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -45,17 +34,10 @@ class MainMeteoFragment : Fragment() {
 
         hide()
 
-<<<<<<< HEAD
-=======
-        binding.Goto.setOnClickListener{  view : View ->
-            view.findNavController().navigate(R.id.action_mainMeteoFragment_to_loginFragment) }
-
->>>>>>> 66331d2c6b518470ccecdcd76e1d77bda278a5ee
         val toolbar = (activity as AppCompatActivity).findViewById<Toolbar>(R.id.toolbar)
 
         toolbar.title = "Ancona"
 
-<<<<<<< HEAD
        /* firebaseAuth = FirebaseAuth.getInstance()
 
         val currentUser : FirebaseUser? = firebaseAuth.currentUser
@@ -65,8 +47,6 @@ class MainMeteoFragment : Fragment() {
             findNavController().navigate(R.id.action_fragment_to_navigation_login)
         } */
 
-=======
->>>>>>> 66331d2c6b518470ccecdcd76e1d77bda278a5ee
         return binding.root
     }
 
