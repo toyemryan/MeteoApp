e8d5d7d4dfde1cdae566b0d5bb6bcbb4dc43f89f
c-ode6968 <lioneleecom@gmail.com>
2023-11-01 03:37:08 +0100

    nextday

--- app/src/main/java/com/example/meteoapp/MainActivity.kt
+++ app/src/main/java/com/example/meteoapp/MainActivity.kt
@@ -108,6 +108,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             R.id.aboutFragment -> {
                 navController.navigate(R.id.action_mainMeteoFragment_to_aboutFragment)
             }
+            R.id.cityFragment ->{
+                navController.navigate(R.id.action_mainMeteoFragment_to_aboutFragment)
+            }
         }
         binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
